dc096dd91b054505cb348100ff7a68e937c44e34
Ben <benvanhartingsveldt@gmail.com>
2020-10-22 18:29:37 +0200

    [Feature] Add Dutch translation files (#563)
    Translate errors and validations to Dutch.

--- resources/lang/nl/errors.php
+++ resources/lang/nl/errors.php
@@ -35,122 +35,122 @@ return [
     */
 
     'unauthorized' => [
-        'title' => 'Unauthenticated',
+        'title' => 'Ongeauthenticeerd',
         'detail' => '',
         'code' => '',
     ],
 
     'forbidden' => [
-        'title' => 'Unauthorized',
+        'title' => 'Ongeautoriseerd',
         'detail' => '',
         'code' => '',
     ],
 
     'token_mismatch' => [
-        'title' => 'Invalid Token',
-        'detail' => 'The token is not valid.',
+        'title' => 'Ongeldig Token',
+        'detail' => 'Het token is niet geldig.',
         'code' => '',
     ],
 
     'member_required' => [
-        'title' => 'Non-Compliant JSON API Document',
-        'detail' => 'The member :member is required.',
+        'title' => 'Niet-Conform JSON API Document',
+        'detail' => 'Het onderdeel :member is vereist.',
         'code' => '',
     ],
 
     'member_object_expected' => [
-        'title' => 'Non-Compliant JSON API Document',
-        'detail' => 'The member :member must be an object.',
+        'title' => 'Niet-Conform JSON API Document',
+        'detail' => 'Het onderdeel :member moet een object zijn.',
         'code' => '',
     ],
 
     'member_identifier_expected' => [
-        'title' => 'Non-Compliant JSON API Document',
-        'detail' => 'The member :member must be a resource identifier.',
+        'title' => 'Niet-Conform JSON API Document',
+        'detail' => 'Het onderdeel :member moet een resource identifier zijn.',
         'code' => '',
     ],
 
     'member_string_expected' => [
-        'title' => 'Non-Compliant JSON API Document',
-        'detail' => 'The member :member must be a string.',
+        'title' => 'Niet-Conform JSON API Document',
+        'detail' => 'Het onderdeel :member moet een string zijn.',
         'code' => '',
     ],
 
     'member_empty' => [
-        'title' => 'Non-Compliant JSON API Document',
-        'detail' => 'The member :member cannot be empty.',
+        'title' => 'Niet-Conform JSON API Document',
+        'detail' => 'Het onderdeel :member kan niet leeg zijn.',
         'code' => '',
     ],
 
     'member_field_not_allowed' => [
-        'title' => 'Non-Compliant JSON API Document',
-        'detail' => 'The member :member cannot have a :field field.',
+        'title' => 'Niet-Conform JSON API Document',
+        'detail' => 'Het onderdeel :member kan niet een veld :field hebben.',
         'code' => '',
     ],
 
     'resource_type_not_supported' => [
-        'title' => 'Not Supported',
-        'detail' => 'Resource type :type is not supported by this endpoint.',
+        'title' => 'Niet Ondersteund',
+        'detail' => 'Resource type :type wordt niet ondersteund door dit endpoint.',
         'code' => '',
     ],
 
     'resource_type_not_recognised' => [
-        'title' => 'Not Supported',
-        'detail' => 'Resource type :type is not recognised.',
+        'title' => 'Niet Ondersteund',
+        'detail' => 'Resource type :type wordt niet herkend.',
         'code' => '',
     ],
 
     'resource_id_not_supported' => [
-        'title' => 'Not Supported',
-        'detail' => 'Resource id :id is not supported by this endpoint.',
+        'title' => 'Niet Ondersteund',
+        'detail' => 'Resource id :id wordt niet ondersteund door dit endpoint.',
         'code' => '',
     ],
 
     'resource_client_ids_not_supported' => [
-        'title' => 'Not Supported',
-        'detail' => 'Resource type :type does not support client-generated IDs.',
+        'title' => 'Niet Ondersteund',
+        'detail' => 'Resource type :type ondersteunt geen  client-gegenereerde IDs.',
         'code' => '',
     ],
 
     'resource_exists' => [
         'title' => 'Conflict',
-        'detail' => 'Resource :id already exists.',
+        'detail' => 'Resource :id bestaat al.',
         'code' => '',
     ],
 
     'resource_not_found' => [
-        'title' => 'Not Found',
-        'detail' => 'The related resource does not exist.',
+        'title' => 'Niet gevonden',
+        'detail' => 'De gerelateerde resource bestaat niet.',
         'code' => '',
     ],
 
     'resource_field_exists_in_attributes_and_relationships' => [
-        'title' => 'Non-Compliant JSON API Document',
-        'detail' => 'The :field field cannot exist as an attribute and a relationship.',
+        'title' => 'Niet-Conform JSON API Document',
+        'detail' => 'Het veld :field kan niet bestaan als een attribuut en een relatie.',
         'code' => '',
     ],
 
     'resource_invalid' => [
-        'title' => 'Unprocessable Entity',
-        'detail' => 'The document was well-formed but contains semantic errors.',
+        'title' => 'Onverwerkbare Entiteit',
+        'detail' => 'Het document was goed opgemaakt, maar bevat semantische fouten.',
         'code' => '',
     ],
 
     'resource_cannot_be_deleted' => [
-        'title' => 'Not Deletable',
-        'detail' => 'The resource cannot be deleted.',
+        'title' => 'Niet Verwijderbaar',
+        'detail' => 'Deze resource kan niet worden verwijderd.',
         'code' => '',
     ],
 
     'query_invalid' => [
-        'title' => 'Invalid Query Parameter',
-        'detail' => 'The request query parameters are invalid.',
+        'title' => 'Ongeldige queryparameter',
+        'detail' => 'De queryparameters van het verzoek zijn ongeldig.',
         'code' => '',
     ],
 
     'failed_validator' => [
-        'title' => 'Unprocessable Entity',
-        'detail' => 'The document was well-formed but contains semantic errors.',
+        'title' => 'Onverwerkbare Entiteit',
+        'detail' => 'Het document was goed opgemaakt, maar bevat semantische fouten.',
         'code' => '',
     ],
 ];
